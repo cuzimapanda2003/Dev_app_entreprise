7a91654973e6d576281186d480f341d7b871f4b0
cuzimapanda2003 <marcblais03@gmail.com>
2026-03-18 10:42:11 -0400

    demo

---
+++ DemoPersonnesem7Edt/DemoPersonnesem7Edt/ViewModels/MainViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfPersonne.Commands;// ici
+using WpfPersonne.Models;
+
+namespace WpfPersonne.ViewModels
+{
+    public class MainViewModel  :BaseViewModel
+    {   
+       
+        
+        public RelayCommand CmdGotoAccueil { get; private set; }
+        public RelayCommand CmdGotoPersonne { get; private set; }
+
+        private BaseViewModel currentViewModel;
+
+        public BaseViewModel CurrentViewModel
+        {
+            get => currentViewModel;
+            set
+            {
+                currentViewModel = value;
+                OnPropertyChanged();
+            }
+
+        }
+        public MainViewModel()
+        { 
+  
+            CurrentViewModel = new AccueilViewModel();
+            // le contentControl doit savoir afficher un AccueilViewModel
+
+            CmdGotoAccueil = new RelayCommand(GotoAccueil, null);
+            CmdGotoPersonne = new RelayCommand(GotoPersonne, null);
+        }
+        private void GotoPersonne(object obj)
+        {
+            CurrentViewModel = new PersonneViewModel();
+        }
+
+        private void GotoAccueil(object obj)
+        {
+            CurrentViewModel = new AccueilViewModel();
+        }
+    }
+}
